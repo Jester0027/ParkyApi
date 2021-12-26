036baf2b32ecf7f2b8173df0e52edb757409e3fd
Paul <paul.ned@outlook.com>
2021-12-26 19:34:44 +0100

    National park crud controller

--- ParkyAPI/Controllers/NationalParkController.cs
+++ ParkyAPI/Controllers/NationalParkController.cs
@@ -1,8 +1,9 @@
-using System.Collections.Generic;
+using System;
 using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ParkyAPI.Models;
 using ParkyAPI.Models.Dtos;
 using ParkyAPI.Repository.IRepository;
 
@@ -33,6 +34,83 @@ namespace ParkyAPI.Controllers
             var nationalParks = _nationalParkRepository.GetNationalParks();
             var dtos = nationalParks.Select(park => _mapper.Map<NationalParkDto>(park)).ToList();
             return Ok(dtos);
+        }
+
+        [HttpGet("{id:int}", Name = "GetNationalPark")]
+        public IActionResult GetNationalPark(int id)
+        {
+            var np = _nationalParkRepository.GetNationalPark(id);
+            if (np == null)
+            {
+                return NotFound();
+            }
+            var npDto = _mapper.Map<NationalParkDto>(np);
+            return Ok(npDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_nationalParkRepository.NationalParkExists(nationalParkDto.Name))
+            {
+                ModelState.AddModelError("Error", "National park already exists");
+                return BadRequest(ModelState);
+            }
+
+            var nationalPark = _mapper.Map<NationalPark>(nationalParkDto);
+            if (!_nationalParkRepository.CreateNationalPark(nationalPark))
+            {
+                ModelState.AddModelError("Error", $"Something went wrong when saving {nationalPark.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            var date = DateTime.Now;
+            _logger.LogInformation("[{date}] National park added: {nationalPark.Name} ({nationalPark.Id})", date, nationalPark.Name, nationalPark.Id);
+            return CreatedAtRoute("GetNationalPark", new {id = nationalPark.Id}, nationalPark);
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult UpdateNationalPark(int id, [FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null || id != nationalParkDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalPark = _mapper.Map<NationalPark>(nationalParkDto);
+            if (!_nationalParkRepository.UpdateNationalPark(nationalPark))
+            {
+                ModelState.AddModelError("Error", $"Something went wrong when updating {nationalPark.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            var date = DateTime.Now;
+            _logger.LogInformation("[{date}] National park updated: {nationalPark.Name} ({nationalPark.Id})", date, nationalPark.Name, nationalPark.Id);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteNationalPark(int id)
+        {
+            var nationalPark = _nationalParkRepository.GetNationalPark(id);
+            if (nationalPark == null)
+            {
+                return NotFound();
+            }
+
+            if (!_nationalParkRepository.DeleteNationalPark(nationalPark))
+            {
+                ModelState.AddModelError("Error", $"Something went wrong when deleting {nationalPark.Name}");
+                return StatusCode(500, ModelState);
+            }
+            var date = DateTime.Now;
+            _logger.LogInformation("[{date}] National park deleted: {nationalPark.Name} ({nationalPark.Id})", date, nationalPark.Name, nationalPark.Id);
+            return NoContent();
         }
     }
 }
--- ParkyAPI/Models/Dtos/NationalParkDto.cs
+++ ParkyAPI/Models/Dtos/NationalParkDto.cs
@@ -1,11 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace ParkyAPI.Models.Dtos
 {
     public class NationalParkDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string State { get; set; }
         public DateTime Create { get; set; }
         public DateTime Established { get; set; }
